7ed4849ba6e79f5becb9672fdcb09416dd3f6171
Amir Haghighati <amirh2008@gmail.com>
2016-07-14 16:49:27 +0430

    UI updated, materilize added to project

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.master')
 
-@section('title', 'Page Title')
 
 @section('sidebar')
     @parent
@@ -9,5 +8,37 @@
 @endsection
 
 @section('content')
-    <p>This is my body content.</p>
+    <div class="progress">
+        <div class="indeterminate"></div>
+    </div>
+
+    <div class="container">
+        <div class="row pink accent-3">
+            <div class="col s1">1</div>
+            <div class="col s1">2</div>
+            <div class="col s1">3</div>
+            <div class="col s1">4</div>
+            <div class="col s1">5</div>
+            <div class="col s1">6</div>
+            <div class="col s1">7</div>
+            <div class="col s1">8</div>
+            <div class="col s1">9</div>
+            <div class="col s1">10</div>
+            <div class="col s1">11</div>
+            <div class="col s1">12</div>
+        </div>
+        <div class="row">
+            <div class="col s12">
+                <p class="flow-text">This is my body content.</p>
+                <ul id="dropdown" class="dropdown-content">
+                    <li><a href="#!">one<span class="badge">1</span></a></li>
+                    <li><a href="#!">two<span class="new badge">1</span></a></li>
+                    <li><a href="#!">three</a></li>
+                </ul>
+                <a class="btn dropdown-button" href="#!" data-activates="dropdown">Dropdown<i class="mdi-navigation-arrow-drop-down right"></i></a>
+
+            </div>
+        </div>
+    </div>
+
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- To ensure proper rendering and touch zooming -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
@@ -10,7 +10,10 @@
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="{{ URL::asset('css/customized_bootstrap.css') }}" >
-
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="{{ URL::asset('css/materialize.min.css') }}"  media="screen,projection"/>
     <!-- setting favicon -->
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >
 
@@ -23,5 +26,9 @@
         <div class="container">
             @yield('content')
         </div>
+
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="{{URL::asset('js/materialize.min.js')}}"></script>
     </body>
 </html>

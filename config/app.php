abba52482237da461bff7057280135975c301e51
Amir Haghighati <amirh2008@gmail.com>
2017-11-17 18:47:18 +0330

    Initial configs are set

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+	    /*
+	     * IDE HELPER
+	     */
+	    \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*

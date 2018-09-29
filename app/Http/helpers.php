14a932e5b6b894748ad39f88a720e14a41844649
Amir HaghighatiMaleki <ahaghig3@uwo.ca>
2018-09-29 14:44:38 -0400

    cleanup

--- app/Http/helpers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function strip($html)
-{
-	return strip_tags($html, "<a><b><blockquote><code><del><dd><dl><dt><em><h1><h2><h3><h4><h5><h6><i><img><kbd><li><ol><p><pre><s><sup><sub><strong><strike><ul><br><hr>");
-}
--- resources/views/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-    <div class="container text-center">
-        Developed Using <a href="https://getbootstrap.com"><i class="devicons devicons-bootstrap"></i></a>
-    </div>
-</footer>
